032ac90b8599191d0853496720eceec5661f87a9
Jen Annoh <jannoh@ed.ac.uk>
2026-06-25 00:23:14 +0100

    Add standard deviation shading to plot

--- gastruloid_elongation_shiny/app.R
+++ gastruloid_elongation_shiny/app.R
@@ -13,6 +13,13 @@ library(RColorBrewer)
 
 # load data
 df <- read.csv("data/Wnt_3_ways_R2_clean_summary.csv", header = T)
+# calculate standard deviaton for shading
+df <- df %>% 
+  mutate(
+    EI_max = EI_Avg+EI_Sd,
+    EI_low = EI_Avg-EI_Sd
+  )
+
 #get lists of unique cell lines and conditions
 g_groups <- as.list(unique(df$Group))
 g_conditions <- unique(df$Condition)
@@ -68,11 +75,11 @@ server <- function(input, output) {
     p <- df |> filter(Group == input$group, Condition %in% input$condition)  
     # the %in% operator allows none or multiple selections!!, input$condition is basically a vector when multiple choice
     
-    ggplot(p, aes(x = factor(Timepoint, levels=c('72h', '96h', '120h')), y = EI_Avg, group = Condition, colour = Condition)) +
+    ggplot(p, aes(x = factor(Timepoint, levels=c('72h', '96h', '120h')), y = EI_Avg, group = Condition, ymin = EI_low, ymax = EI_max, fill = Condition, colour = Condition)) +
+      scale_color_manual(values =  condition_colours, aesthetics = c("colour", "fill")) +
       geom_line(linewidth = 2) +
-      # geom_ribbon(alpha=0.25) + # add error shading
+      geom_ribbon(alpha=0.2, linewidth = 0) +
       theme_minimal() +
-      scale_color_manual(values =  condition_colours) +
       ggtitle(paste(input$group, " Elongation Index")) +
       ylab("Elongation Index") +
       xlab("Timepoint")
